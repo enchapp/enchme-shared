1366efde6b230d9b0c85a2b61475744d783d8c22
artmadiar <artmadiar@gmail.com>
2020-03-10 20:57:22 +0100

    update default setting

--- examples/index.js
+++ examples/index.js
@@ -1,43 +1,26 @@
 module.exports.defaultPage = {
   settings: {
-    fonts: {
-      Heading: {
-        fontFamily: 'Helvetica',
-        fontSize: { minValue: 30, maxValue: 40, value: "35" },
-        color: {
-          current: '225,87,87',
-          lastColors: ['225,87,87']
-        }
-      },
-      Title: {
-        fontFamily: 'Lato',
-        fontSize: { minValue: 20, maxValue: 30, value: "25" },
-        color: {
-          current: '245,245,245',
-          lastColors: ['245,245,245']
-        }
-      },
-      Description: {
-        fontFamily: 'Lato',
-        fontSize: { minValue: 10, maxValue: 20, value: "15" },
-        color: {
-          current: '164,164,164',
-          lastColors: ['164,164,164']
-        }
+    fontFamily: 'PLAYFAIR DISPLAY',
+    shadow: {
+      name: 'hard',
+      value: {
+        positionX: 5,
+        positionY: 5,
+        blur: 0,
+        spread: 0,
+        color: '#000000',
+        active: true
       }
     },
-    styles: {
-      shape: {
-        name: 'Rounded',
-        isFully: false,
-        radius: '3px'
-      },
-      buttonStyle: { type: 'NORMAL' },
-      backgroundView: {
-        type: "COLOR",
-        color: '48,63,159'
-      },
-      mainColor: '63,81,181'
+    mainColor: '#EB5757',
+    shape: {
+      name: "oval",
+      value: "100"
+    },
+    background: {
+      color: '',
+      type: 'PATTERN',
+      src: '/static/media/3-min.8401dd8e.jpg'
     }
   },
   components: [],
